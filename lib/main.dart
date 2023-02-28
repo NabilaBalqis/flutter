7d700d48dba7acdf2bcfe6a7c192a3e72589aa9b
NabilaBalqis <nabilabalqis133@gmail.com>
2023-02-28 09:25:02 +0700

    Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      MaterialApp(
+          title: "Aplikasi Flutter",
+          home: Scaffold(
+            appBar: AppBar(title: Text('Aplikasi Flutter'),),
+            body: Material(
+              color: Colors.white,
+              child: Center(
+                child: Text(
+                  "Hello World",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(color: Colors.black, fontSize: 40.0),
+                ),
+              ),
+            ),
+          )
+      ));
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
